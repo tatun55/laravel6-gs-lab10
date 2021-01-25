db2242efe452c0575898e85f083b623d038907fa
tatun55 <tatun55@gmail.com>
2021-01-25 18:37:12 +0900

    演習4-2 : 編集リクエストのバリデーション 最終形態

--- backend/app/Http/Requests/BookRequest.php
+++ backend/app/Http/Requests/BookRequest.php
@@ -9,24 +9,25 @@ class BookRequest extends FormRequest
 {
     public function rules()
     {
+        //共通のルール
+        $rules = [
+            'item_name' => 'required|string|min:3|max:255',
+            'item_number' => 'required|integer|min:1|max:100',
+            'item_amount' => 'required|integer|min:100|max:100000',
+            'published' => 'required|date|before_or_equal:' . Carbon::today(),
+        ];
+
         $route = $this->route()->getName();
         switch ($route) {
             case 'books.store':
-                $rules = [
-                    'item_name' => 'required|string|min:3|max:255',
-                    'item_number' => 'required|integer|min:1|max:100',
-                    'item_amount' => 'required|integer|min:100|max:100000',
-                    'published' => 'required|date|before_or_equal:' . Carbon::today(),
+                // 新規登録独自のルール
+                $storeRules = [
                     'item_img' => 'file|image|dimensions:min_width=100,min_height=100',
                 ];
+                // 共通のルールと独自ルールをマージ
+                $rules = array_merge($rules, $storeRules);
                 break;
             case 'books.update':
-                $rules = [
-                    'item_name' => 'required|string|min:3|max:255',
-                    'item_number' => 'required|integer|min:1|max:100',
-                    'item_amount' => 'required|integer|min:100|max:100000',
-                    'published' => 'required|date|before_or_equal:' . Carbon::today(),
-                ];
                 break;
         }
 

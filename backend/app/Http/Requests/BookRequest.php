6e71945bbfe080c5ffa56b9b9db50b135c31ae94
tatun55 <tatun55@gmail.com>
2021-01-25 19:01:38 +0900

    演習5-2 : Uniqueルール ※ 正解バージョン

--- backend/app/Http/Requests/BookRequest.php
+++ backend/app/Http/Requests/BookRequest.php
@@ -11,7 +11,6 @@ class BookRequest extends FormRequest
     {
         //共通のルール
         $rules = [
-            'item_name' => 'required|string|min:3|max:255|unique:books',
             'item_number' => 'required|integer|min:1|max:100',
             'item_amount' => 'required|integer|min:100|max:100000',
             'published' => 'required|date|before_or_equal:' . Carbon::today(),
@@ -22,12 +21,20 @@ class BookRequest extends FormRequest
             case 'books.store':
                 // 新規登録独自のルール
                 $storeRules = [
+                    'item_name' => 'required|string|min:3|max:255|unique:books',
                     'item_img' => 'file|image|dimensions:min_width=100,min_height=100',
                 ];
                 // 共通のルールと独自ルールをマージ
                 $rules = array_merge($rules, $storeRules);
                 break;
             case 'books.update':
+                // 編集独自のルール
+                $updateRules = [
+                    'item_name' => 'required|string|min:3|max:255|unique:books,item_name,' . $this->id, // 今回は$this->idでIDを取得できるが違うことが多い
+                    'item_img' => 'file|image|dimensions:min_width=100,min_height=100',
+                ];
+                // 共通のルールと独自ルールをマージ
+                $rules = array_merge($rules, $updateRules);
                 break;
         }
 

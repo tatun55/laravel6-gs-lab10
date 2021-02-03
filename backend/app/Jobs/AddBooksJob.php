ef1ec62b291c27530a83d0627106763fca57e3a4
tatun55 <tatun55@gmail.com>
2021-02-03 17:22:40 +0900

    STEP1 : 本を追加するジョブ

---
+++ backend/app/Jobs/AddBooksJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use AddBooksSeeder;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AddBooksJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $seeder = new AddBooksSeeder();
+        $seeder->run();
+        \Log::info('本が10冊追加されたよ');
+    }
+}
